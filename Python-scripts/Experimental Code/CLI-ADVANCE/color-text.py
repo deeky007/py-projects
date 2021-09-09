ce124efab3ca639499783184d75f951d95cae185
Dev Shakya <shakyatechnicals@gmail.com>
2021-09-09 16:31:39 +0530

    Create color-text.py

---
+++ Python-scripts/Experimental Code/CLI-ADVANCE/color-text.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Dev Shakya"+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
+print(Back.CYAN+"Hi My name is Nirdesh Tyagi")
+print(Fore.RED + Back.GREEN+ "I am your python Instructor")
